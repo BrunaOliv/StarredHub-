26d7f4e4c075ec50646615437b6e6e21e0d404de
Bruna <brunasoaresoliv@outlook.com>
2025-04-05 16:38:37 -0300

    estilizando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,33 +1,50 @@
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
-import {Container} from './styles';
+import {Container, Owner, Loading, BackButton} from './styles';
 import api from '../../services/api';
+import {FaArrowLeft} from 'react-icons/fa'
 
 export default function Repositorio(){
-    const { repositorioRota } = useParams();
-    const [repositorio, setRepositorios] = useState({});
+    const { repositorio } = useParams();
+    const [repositorioSelecionado, setRepositorios] = useState({});
     const [issues, setIssues] = useState([]);
     const [loading, setLoading] = useState(true);
  
     useEffect(() => {
         async function load(){
             const [repositorioData, issueData] = await Promise.all([
-                api.get(`/repos/${repositorioRota}`),
-                api.get(`repos/${repositorioRota}/issues`,  {params: {state: 'open', per_page: 5}})
+                api.get(`/repos/${repositorio}`),
+                api.get(`repos/${repositorio}/issues`,  {params: {state: 'open', per_page: 5}})
             ])
-
-            setRepositorios(repositorioData);
-            setIssues(issueData);
+            console.log(repositorioData.data);
+            setRepositorios(repositorioData.data);
+            setIssues(issueData.data);
             setLoading(false);
         }
 
         load();
-    }, [repositorioRota])
+    }, [repositorio]);
 
+
+    if(loading){
+        return(
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        )
+    }
 
     return(
         <Container>
-            
+            <BackButton to="/">
+                <FaArrowLeft color="#000" size={30} />
+            </BackButton>
+
+            <Owner>
+                <img src={repositorioSelecionado.owner.avatar_url} alt={repositorioSelecionado.owner.login} />
+                <h1>{repositorioSelecionado.name}</h1>
+                <p>{repositorioSelecionado.description}</p>
+            </Owner>
         </Container>
     )
 }
