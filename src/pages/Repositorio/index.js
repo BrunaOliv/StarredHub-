e2b2b2bae9b0deff0785ac4e47e755f3c5cd81c0
Bruna <brunasoaresoliv@outlook.com>
2025-04-05 15:53:58 -0300

    add detalhes repositorios

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+import {Container} from './styles';
+import api from '../../services/api';
 
 export default function Repositorio(){
+    const { repositorioRota } = useParams();
+    const [repositorio, setRepositorios] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+ 
+    useEffect(() => {
+        async function load(){
+            const [repositorioData, issueData] = await Promise.all([
+                api.get(`/repos/${repositorioRota}`),
+                api.get(`repos/${repositorioRota}/issues`,  {params: {state: 'open', per_page: 5}})
+            ])
+
+            setRepositorios(repositorioData);
+            setIssues(issueData);
+            setLoading(false);
+        }
+
+        load();
+    }, [repositorioRota])
+
+
     return(
-        <h1>Repositorio</h1>
+        <Container>
+            
+        </Container>
     )
 }
